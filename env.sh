ba29f2fdfbcca10cb158be8ebb36da330c94e189
Dimitrios Chasapis <dchasapi@bsc.es>
2024-11-08 11:56:15 +0100

    Fixed TRACE_DIR path

--- env.sh
+++ env.sh
@@ -1,8 +1,7 @@
 
 export ROOT_DIR=`pwd`
 export CHAMPSIM_DIR=${ROOT_DIR}/ChampSim
-#export TRACE_DIR=${ROOT_DIR}/traces
-export TRACES_DIR="/home/bsc/bsc018186/romol/ChampSim/traces"
+export TRACE_DIR=${ROOT_DIR}/traces
 export DUMP_DIR=${ROOT_DIR}/dump
 export STATS_DIR=${STATS_DIR}/stats
 FIGURES_DIR=${ROOT_DIR}/figures
