5e4b3a0e8a1a56a8bf7ad48b716a64a8dcccf6ed
dchasap <50141180+dchasap@users.noreply.github.com>
2024-12-06 13:22:03 +0100

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,8 +1,7 @@
 
 export ROOT_DIR=`pwd`
 export CHAMPSIM_DIR=${ROOT_DIR}/ChampSim
-#export TRACE_DIR=${ROOT_DIR}/traces
-export TRACE_DIR=/gpfs/projects/bsc18/MN4/bsc18/romol/ChampSim/traces
+export TRACE_DIR=${ROOT_DIR}/traces
 export DUMP_DIR=${ROOT_DIR}/dump
 export STATS_DIR=${STATS_DIR}/stats
 export FIGURES_DIR=${ROOT_DIR}/figures
